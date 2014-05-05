24fdbf02b36fb3c188af33a6c978606265874dc3
Paul Morrison <paul.unr@gmail.com>
2014-05-05 11:40:46 -0400

    General QOL fixes.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias ngrok='~/ngrok -subdomain=iamunr 80'
 alias viminstall='vim +BundleInstall +qall'
 alias vimclean='vim +BundleClean +qall'
 alias l='ls -la'
+alias j='jump'
 
 alias mysql='/Applications/MAMP/Library/bin/mysql'
 
